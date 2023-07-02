569000cfa7239bb1d8057fab5e01e0a4d9fcb3dc
putuagra <iputuagra@gmail.com>
2023-07-02 19:01:29 +0700

    Add GetBookingDetails

--- API/Repositories/BookingRepository.cs
+++ API/Repositories/BookingRepository.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using API.DTOs.Bookings;
 using API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Repositories;
 
@@ -10,6 +11,26 @@ public class BookingRepository : GeneralRepository<Booking>, IBookingRepository
     public BookingRepository(BookingDbContext context) : base(context)
     {
 
+    }
+
+    public IEnumerable<BookingDetailsDto> GetBookingDetails()
+    {
+        var bookings = _context.Bookings.Include(b => b.Room)
+            .Include(b => b.Employee)
+            .Where(b => b.Room.Guid == b.Room.Guid && b.Employee.Guid == b.Employee.Guid).ToList();
+
+        var bookingDetails = bookings.Select(b => new BookingDetailsDto
+        {
+            Guid = b.Guid,
+            BookedNik = b.Employee.Nik,
+            BookedBy = b.Employee.FirstName + "" + b.Employee.LastName,
+            RoomName = b.Room.Name,
+            StartDate = b.StartDate,
+            EndDate = b.EndDate,
+            Status = b.Status,
+            Remarks = b.Remarks
+        });
+        return bookingDetails;
     }
 
     public ICollection<GetRoomTodayDto>? GetByDateNow()
