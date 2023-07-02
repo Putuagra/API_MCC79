cd157e640a5ba2bbc65942ac60a26b76bb07c645
putuagra <iputuagra@gmail.com>
2023-07-02 18:29:17 +0700

    Add GetLastEmployeeNik

--- API/Contracts/IEmployeeRepository.cs
+++ API/Contracts/IEmployeeRepository.cs
@@ -7,4 +7,5 @@ public interface IEmployeeRepository : IGeneralRepository<Employee>
     public Employee? GetByEmailAndPhoneNumber(string nama);
     public Employee? GetByEmail(string email);
     public Employee? GetEmailLogin(string email);
+    string? GetLastEmployeeNik();
 }
